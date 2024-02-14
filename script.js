dfa81513ea2a21848cf7853f99639ae37bfec146
Nojamcinnamon <159984719+Nojamcinnamon@users.noreply.github.com>
2024-02-14 12:32:40 +0800

    Update script.js

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ noButton.addEventListener("click", function () {
 });
 
 function handleYesClick() {
-  titleElement.innerHTML = "Yayyy!! :3";
+  titleElement.innerHTML = "Yayyy!! Happy Valentine's Baby! <3";
   buttonsContainer.classList.add("hidden");
   changeImage("yes");
 }
